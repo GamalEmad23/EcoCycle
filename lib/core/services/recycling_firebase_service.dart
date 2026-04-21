6468577d57b9cb1eb8763da32716688c167c1de5
gamal emad <gemad232005@gmail.com>
2026-04-21 23:06:15 +0200

    feat: add RecyclingFirebaseService to handle recycling request uploads to Firestore

--- lib/core/services/recycling_firebase_service.dart
+++ lib/core/services/recycling_firebase_service.dart
@@ -1,15 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:eco_cycle/features/recycling_request/model/recycling_request_model.dart';
 
 class RecyclingFirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> uploadRecyclingRequest({
     required RecyclingRequestModel requestModel,
   }) async {
     try {
-      // Save data directly to Firestore without uploading image to Storage
-      await _firestore.collection('recycling_requests').add(requestModel.toMap());
+      final userId = _auth.currentUser?.uid;
+      
+      if (userId == null) {
+        throw Exception('User must be logged in to create a request');
+      }
+
+      // Save data to the user's specific subcollection
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('recycling_requests')
+          .add(requestModel.toMap());
     } catch (e) {
       throw Exception('Failed to create request: $e');
     }
